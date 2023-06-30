739973263360147b9bd046ef7408aff22c883391
LeMinhDucDN <duc.le@madison-technology.com>
2023-06-30 17:36:21 +0700

    Update code add demo

---
+++ script.js
@@ -0,0 +1,27 @@
+
+var buttonInfo = [
+    { label: "Age Calculator", filePath: "project_demo/age_calculator/age-index.html" },
+    { label: "Amine Pics", filePath: "project_demo/amine_pics_generator/amine-index.html" },
+    { label: "Button 3", filePath: "file3.html" }
+  ];
+  
+  var listContainer = document.createElement("div");
+  listContainer.className = "list-container";
+  
+  buttonInfo.forEach(function(button) {
+    var buttonElement = document.createElement("button");
+    buttonElement.textContent = button.label;
+    buttonElement.onclick = function() {
+      openFile(button.filePath);
+    };
+  
+    listContainer.appendChild(buttonElement);
+  });
+  
+  var rootElement = document.getElementById("root");
+  rootElement.appendChild(listContainer);
+  
+  function openFile(filePath) {
+    window.location.href = filePath;
+  }
+  
